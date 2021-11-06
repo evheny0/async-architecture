4493a6a9ba2b10ebc18a1517c170244e028f682f
Eugene Haisenka <evheny0@gmail.com>
2021-11-06 23:40:38 +0300

    Add events for tasks

--- tasks/app/controllers/personal_tasks_controller.rb
+++ tasks/app/controllers/personal_tasks_controller.rb
@@ -7,6 +7,25 @@ class PersonalTasksController < ApplicationController
     task = Task.find(params[:id])
     task.update!(status: :done)
 
+
+    event = {
+      event_name: 'TaskCompleted',
+      data: {
+        public_id: task.public_id,
+        status: task.status,
+      }
+    }
+    WaterDrop::SyncProducer.call(event.to_json, topic: 'tasks')
+
+    event = {
+      event_name: 'TaskUpdated',
+      data: {
+        public_id: task.public_id,
+        status: task.status,
+      }
+    }
+    WaterDrop::SyncProducer.call(event.to_json, topic: 'tasks-stream')
+
     redirect_to "/"
   end
 end
