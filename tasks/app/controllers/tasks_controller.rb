73213d5761a466a901eebee2bf13434d0b190173
Eugene Haisenka <evheny0@gmail.com>
2021-11-06 19:40:06 +0300

    Add assignee for task

--- tasks/app/controllers/tasks_controller.rb
+++ tasks/app/controllers/tasks_controller.rb
@@ -7,17 +7,17 @@ class TasksController < ApplicationController
   end
 
   def create
-    Task.create!(task_params.merge(public_id: SecureRandom.uuid))
+    Task.create!(task_params.merge(public_id: SecureRandom.uuid, status: :created))
 
     redirect_to tasks_path
   end
 
   def edit
-    @task = Task.find_by(params[:id])
+    @task = Task.find(params[:id])
   end
 
   def update
-    @task = Task.find_by(params[:id])
+    @task = Task.find(params[:id])
     @task.update!(task_params)
 
     redirect_to tasks_path
--- tasks/app/models/task.rb
+++ tasks/app/models/task.rb
@@ -1,2 +1,5 @@
 class Task < ApplicationRecord
+  belongs_to :assignee, class_name: "User"
+  # belongs_to :creator, class_name: "User"
+  enum status: %i(created done)
 end
