2f814c93264416946050ffdce5e609308704a4f0
Eugene Haisenka <evheny0@gmail.com>
2021-11-14 14:50:16 +0300

    Add new statuses

--- tasks/app/models/task.rb
+++ tasks/app/models/task.rb
@@ -1,5 +1,5 @@
 class Task < ApplicationRecord
   belongs_to :assignee, class_name: "User"
   belongs_to :creator, class_name: "User"
-  enum status: %i(created done)
+  enum status: %i(created done ptichka_v_kletke proso_v_miske)
 end
